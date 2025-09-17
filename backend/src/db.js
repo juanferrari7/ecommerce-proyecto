077c671610383b6f51bac7ae54e7995a95140fd0
juanferrari7 <juan.ferrari304@gmail.com>
2025-09-17 19:33:33 -0300

    Conectando sequelize

---
+++ backend/src/db.js
@@ -0,0 +1,15 @@
+import Sequelize from 'sequelize'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const {DB_NAME, DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_DIALECT} = process.env
+
+export const sequelize = new Sequelize(
+  DB_NAME,
+  DB_USER,
+  DB_PASS, {
+    host: DB_HOST,
+    dialect: DB_DIALECT,
+    port: DB_PORT
+  }
+)
